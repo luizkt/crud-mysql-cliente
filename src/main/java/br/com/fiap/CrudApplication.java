a390a0a9d0563e22c07f740791230937aa92da43
luizkt <keese.luiz@gmail.com>
2019-08-27 19:57:33 -0300

    Ajuste no nome do banco

--- src/main/java/br/com/fiap/CrudApplication.java
+++ src/main/java/br/com/fiap/CrudApplication.java
@@ -4,16 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-//@ComponentScan("br.com.fiap")
-@EnableJpaRepositories(basePackages={"br.com.fiap.repository"})
-@EntityScan(basePackages="br.com.fiap.entity")
-@ComponentScan(basePackages={"br.com.fiap"})
+@Configuration
+@PropertySource("application.properties")
+@EnableJpaRepositories(basePackages = { "br.com.fiap.repository" })
+@EntityScan(basePackages = "br.com.fiap.entity")
+@ComponentScan(basePackages = { "br.com.fiap" })
 public class CrudApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CrudApplication.class, args);
 	}
 }
