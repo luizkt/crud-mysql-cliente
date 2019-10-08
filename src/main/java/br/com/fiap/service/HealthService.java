d866c044f57076093c3082de0839e707a87b6dfe
luizkt <keese.luiz@gmail.com>
2019-10-08 18:21:34 -0300

    Adicionado endpoint de health check

--- src/main/java/br/com/fiap/service/HealthService.java
+++ src/main/java/br/com/fiap/service/HealthService.java
@@ -4,8 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class IndexService {
-    @GetMapping("/")
+public class HealthService {
+    @GetMapping("/health")
     public String index() {
         return "Hello there! I'm running.";
     }
